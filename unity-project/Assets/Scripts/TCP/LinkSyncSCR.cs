a224bc4e3184e576272c934c9a95df3d850fb4eb
Benjamin Führmann <benjamin.fuhrmann@esial.net>
2013-02-06 17:54:57 +0100

    TCP/IP Server/Client and begin of the middleware

---
+++ unity-project/Assets/Scripts/TCP/LinkSyncSCR.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+ 
+public class LinkSyncSCR : MonoBehaviour {
+	public Connector test=new Connector();
+	string lastMessage;
+	public Transform PlayerCoord;
+ 
+	void Start () 
+	{
+		Debug.Log(test.fnConnectResult("127.0.0.1",10000,System.Environment.MachineName));
+		if (test.res !="")
+		{
+			Debug.Log(test.res);
+		}
+ 
+	}
+	void Update () 
+	{
+		if (Input.GetKeyDown ("space")) 
+		{
+	        Debug.Log ("space key was pressed");
+			test.fnPacketTest("space key was pressed");
+		}
+ 
+		if (Input.GetKeyDown ("escape")) 
+		{
+	        Debug.Log ("escape key was pressed");
+			test.fnPacketTest("escape key was pressed");
+		}
+		if (test.strMessage !="JOIN")
+		{
+			if (test.res != lastMessage)
+			{
+				Debug.Log(test.res);
+				lastMessage=test.res;
+			}
+		}
+		test.fnPacketTest(PlayerCoord.position[0]+","+PlayerCoord.position[1]+","+PlayerCoord.position[2]);
+	}
+ 
+	void OnApplicationQuit (){
+		try{test.fnDisconnect();}
+		catch{}
+	}
+}
---
+++ veryseriousmiddleware/src/TCPServer.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class TCPServer {
+	
+	   public static void main(String argv[]) throws Exception {
+	         String clientSentence;
+	         //String capitalizedSentence;
+	         ServerSocket welcomeSocket = new ServerSocket(10000);
+
+        	 System.out.println("lol");
+	            Socket connectionSocket = welcomeSocket.accept();
+	         while(true)
+	         {
+	            System.out.println("lal");
+	            BufferedReader inFromClient =
+	               new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+	            //DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+	            clientSentence = inFromClient.readLine();
+	            System.out.println("Received: " + clientSentence);
+	            //capitalizedSentence = clientSentence.toUpperCase() + '\n';
+	            //outToClient.writeBytes(capitalizedSentence);
+	         }
+	         
+	      }
+	}
